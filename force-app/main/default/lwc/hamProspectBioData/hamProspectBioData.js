2ff9bdcaf13b906ffb4f23d70909a8a89fdf32c0
nvaidya@hamilton.edu.ham.prod <nvaidya@hamilton.edu>
2025-10-28 15:43:03 +0000

    [Copado] [ASC-9956] ASC-9956 - JEDI Make children as links

--- force-app/main/default/lwc/hamProspectBioData/hamProspectBioData.js
+++ force-app/main/default/lwc/hamProspectBioData/hamProspectBioData.js
@@ -1,11 +1,13 @@
 import { LightningElement, api, track, wire } from 'lwc';
 
 import searchProspectDetails from '@salesforce/apex/HAMProspectOverviewApexController.searchProspectDetails';
+import getChildrenLinks from '@salesforce/apex/HAMProspectOverviewApexController.getChildrenLinks';
 import { CurrentPageReference } from 'lightning/navigation';
+import { NavigationMixin } from 'lightning/navigation';
 
 
-export default class HamProspectBioData extends LightningElement {
-    @api contactrecord; 
+export default class HamProspectBioData extends NavigationMixin(LightningElement) {
+    @api contactrecord;
     @track activeSectionBio = 'Demographics';
     @track biorecordId= '';
     @api recordId;
@@ -13,9 +15,10 @@ export default class HamProspectBioData extends LightningElement {
     @track chapterLink = '';
     @track prmLink = '';
     @track spouseLink = '';
+    @track childrenLinks = [];
 
     connectedCallback() {
-       
+
         if (this.recordId) {
             searchProspectDetails({ contactId: this.recordId})
                 .then(result => {
@@ -29,10 +32,52 @@ export default class HamProspectBioData extends LightningElement {
                     }if(this.contactrecord.ucinn_ascendv2__Preferred_Spouse__c){
                         this.spouseLink = '/lightning/r/contact/' + this.contactrecord.ucinn_ascendv2__Preferred_Spouse__c + '/view';
                     }
+
+                    // Load children links if HAM_Children__c has value
+                    if(this.contactrecord.HAM_Children__c){
+                        this.loadChildrenLinks(this.contactrecord.HAM_Children__c);
+                    }
                 })
                 .catch(error => {
                     console.error('Error fetching Prospect:', error);
                 });
+        }
+    }
+
+    // Load children links from Apex
+    loadChildrenLinks(childrenString) {
+        getChildrenLinks({ childrenString: childrenString })
+            .then(result => {
+                // Add isLast property for comma separation
+                const children = result || [];
+                this.childrenLinks = children.map((child, index) => ({
+                    ...child,
+                    isLast: index === children.length - 1
+                }));
+            })
+            .catch(error => {
+                console.error('Error loading children links:', error);
+                this.childrenLinks = [];
+            });
+    }
+
+    // Navigate to child's JEDI Overview page
+    handleChildClick(event) {
+        const childId = event.target.dataset.childId;
+        if (childId) {
+            this[NavigationMixin.GenerateUrl]({
+                type: 'standard__navItemPage',
+                attributes: {
+                    apiName: 'JEDI_Overview'
+                },
+                state: {
+                    c__recordId: childId
+                }
+            }).then(url => {
+                window.open(url, '_blank', 'noopener,noreferrer');
+            }).catch(error => {
+                console.error('Navigation error:', error);
+            });
         }
     }
 
